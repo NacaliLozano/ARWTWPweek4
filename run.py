1f2cba79be3f883114add9357fc3559c1dd39201
Nacali Lozano <nakaly@gmail.com>
2023-09-08 18:34:05 +0100

    Add run.py

---
+++ run.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This script processes all the data stored in text files into a JSON dictionary"""
+import os
+import requests
+os.chdir("/supplier-data/descriptions")
+for file in os.listdir():
+    try:
+        with open(file) as f:
+            lines = f.readlines()
+            dictionary = {}
+            dictionary["name"] = str(lines[0]).strip()
+            dictionary["weigh"] = int(str(lines[1]).strip()[:-4])
+            dictionary["description"] = str(lines[2:]).strip()
+            response = requests.post("http://[linux-instance-external-IP]/fruits", json=dictionary)
+            print(response.status_code)
+            print(response.request.body)
+    except OSError:
+        print("Error opening {}.".format(file))
