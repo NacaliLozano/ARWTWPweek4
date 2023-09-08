627919cbb1ac3cb4911b961ac6317d4a51f9efed
Nacali Lozano <nakaly@gmail.com>
2023-09-08 18:39:56 +0100

    Add image_name field to the JSON dictionary

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ for file in os.listdir():
             dictionary["name"] = str(lines[0]).strip()
             dictionary["weigh"] = int(str(lines[1]).strip()[:-4])
             dictionary["description"] = str(lines[2:]).strip()
+            dictionary["image_name"] = file[:-4] + ".jpeg"
             response = requests.post("http://[linux-instance-external-IP]/fruits", json=dictionary)
             print(response.status_code)
             print(response.request.body)
