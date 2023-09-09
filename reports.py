fb852aa781b14fd77a6afbb98a0c826a9d826455
Nacali Lozano <nakaly@gmail.com>
2023-09-09 13:12:52 +0100

    Change generate_report function to behave according to parameters

--- reports.py
+++ reports.py
@@ -1,18 +1,11 @@
 """This script generates a report PDF file with the parameters attachment_path, title and paragraph"""
 #!/usr/bin/env python3
 from reportlab.lib.styles import getSampleStyleSheet
-from datetime import date
-import os
-import requests
+from reportlab.platypus import SimpleDocTemplate
+
 def generate_report(attachment_path, title, paragraph):
     styles = getSampleStyleSheet()
-    report_title = Paragraph("Processed Update on " + today.strftime("%B %d, %Y"), styles["h1"])
-    os.chdir("/supplier-data/descriptions")
-    report_body = [Paragraph("<br/>", styles["Normal"])]
-    for file in os.listdir():
-        try:
-            with open(file) as f:
-                lines = f.readlines()
-                report_body.append(Paragraph("name: " + str(lines[0]).strip() + "<br/>", styles["Normal"]))
-                report_body.append(Paragraph("weight: " + str(lines[1]).strip() + "<br/>", styles["Normal"]))
-                report_body.append(Paragraph("<br/>", styles["Normal"])
+    report_title = Paragraph(title, styles["h1"])
+    report_body = Paragraph(paragraph, styles["Normal"])
+    report = SimpleDocTemplate(attachment_path)
+    report.build([report_title, report_body])
