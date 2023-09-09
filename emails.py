ffcb06c958acac9313ae8753105d5d4ad4fdc68c
Nacali Lozano <nakaly@gmail.com>
2023-09-09 17:58:46 +0100

    Add generate_email and send_email functions in the emails.py file

--- emails.py
+++ emails.py
@@ -1,11 +1,27 @@
-    styles = getSampleStyleSheet()
-    report_title = Paragraph("Processed Update on " + today.strftime("%B %d, %Y"), styles["h1"])
-    os.chdir("/supplier-data/descriptions")
-    report_body = [Paragraph("<br/>", styles["Normal"])]
-    for file in os.listdir():
-        try:
-            with open(file) as f:
-                lines = f.readlines()
-                report_body.append(Paragraph("name: " + str(lines[0]).strip() + "<br/>", styles["Normal"]))
-                report_body.append(Paragraph("weight: " + str(lines[1]).strip() + "<br/>", styles["Normal"]))
-                report_body.append(Paragraph("<br/>", styles["Normal"])
+from email.message import EmailMessage
+import os.path
+import mimetypes
+import smtplib
+
+def generate_email(sender, recipient, subject, body, attachment_path):
+    """Returns an email message configured with the input parameters"""
+    message = EmailMessage()
+    message['From'] = sender
+    message['To'] = recipient
+    message['Subject'] = subject
+    message.set_content(body)
+    mime_type, _ = mimetypes.guess_type(attachment_path)
+    mime_type, mime_subtype = mime_type.split('/', 1)
+    with open(attachment_path, 'rb') as ap:
+        message.add_attachment(ap.read(),
+                               maintype=mime_type,
+                               subtype=mime_subtype,
+                               filename=os.path.basename(attachment_path))
+    print(message)
+    return message
+
+def send_email(message):
+    """Sends the message to the configured SMTP server"""
+    mail_server = smtplib.SMTP("localhost")
+    mail_server.send_message(message)
+    mail_server.quit()
